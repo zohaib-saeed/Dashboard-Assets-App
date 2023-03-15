72fe5a4593960c77cc59c4e62044ed343f7bea8d
zohaibzyebi <zyebi553@gmail.com>
2023-03-15 12:54:07 +0500

    customization: tailwind.config

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div className="">Home</div>;
+  return (
+    <div className="text-white w-screen h-screen flex items-center justify-center">
+      Home
+    </div>
+  );
 }
